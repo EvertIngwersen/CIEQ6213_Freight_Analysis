72ea48061577a989af5913638b51e5a11c434f1f
Evert Ingwersen <everti2000@gmail.com>
2025-04-29 13:23:24 +0200

    calc flow

--- excel_toy_python.py
+++ excel_toy_python.py
@@ -1,7 +1,7 @@
 
 #Excel toy test file
 
-
+import math
 import numpy as np
 import pandas as pd
 
@@ -17,7 +17,7 @@ province_list = ['Groningen',
                  'Noord-Brabant',
                  'Limburg']
 
-data = [
+OD = [
     [15, 60, 94, 105, 55, 136, 131, 253, 342, 253, 366],
     [56, 15, 92, 98, 56, 155, 124, 230, 325, 256, 428],
     [101, 95, 15, 48, 103, 106, 151, 144, 253, 242, 283],
@@ -31,17 +31,20 @@ data = [
     [430, 436, 293, 240, 189, 159, 216, 190, 147, 95, 15],
 ]
 
+β = 0.01
+cost_values = np.array(OD)
 
-cost_values = np.array(data)
-
-c_ij = pd.DataFrame(data, index=province_list, columns=province_list)
+c_ij = pd.DataFrame(OD, index=province_list, columns=province_list)
 
 A_i = np.array([124, 26, 19, 97, 157, 38, 182, 290, 75, 98, 114])
 B_j = np.array([41, 19, 15, 69, 95, 33, 127, 177, 43, 78, 59])
 
+flow_matrix = np.zeros((len(province_list),len(province_list)))
 
-
-
+for i in range(len(province_list)):
+    for j in range(len(province_list)):
+        flow_matrix[i,j] = A_i[i]*B_j[j]*math.exp(OD[i,j]*β)
+        
 
 
 
