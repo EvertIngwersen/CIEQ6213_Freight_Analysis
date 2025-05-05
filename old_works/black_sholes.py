44855324d7c44cb2e1f92c165802663b7196aa4c
Evert Ingwersen <everti2000@gmail.com>
2025-05-05 13:30:13 +0200

    wip black sholes

--- old_works/black_sholes.py
+++ old_works/black_sholes.py
@@ -5,11 +5,15 @@ Created on Mon May  5 13:04:06 2025
 @author: evert
 """
 
+import yfinance as yf
+
 from scipy.stats import norm
 from math import log
 from math import sqrt
 from math import exp
+from datetime import datetime
 
+r = 0.05 #risk free interest
 
 def black_scholes(S, K, T, r, sigma, option_type='call'):
     d1 = (log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * sqrt(T))
@@ -20,5 +24,12 @@ def black_scholes(S, K, T, r, sigma, option_type='call'):
     else:
         return K * exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
     
+def find_mispriced_options(ticker, r):
+    stock = yf.Ticker(ticker)
+    spot_price = stock.history(period="1d")['Close'][-1]
+    print(f"\nChecking options for {ticker} - Spot price: ${spot_price:.2f}")
+    for expiry in stock.options[:1]:
+        options_chain = stock.option_chain(expiry)
+        
     
     
