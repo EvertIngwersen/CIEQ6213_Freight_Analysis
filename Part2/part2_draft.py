cfa1d40d2290483e2ee5ba916cf53e2d322e3250
Evert Ingwersen <everti2000@gmail.com>
2025-05-06 12:45:51 +0200

    started part 2

---
+++ Part2/part2_draft.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  6 12:43:57 2025
+
+@author: evert
+"""
+
+import pandas as pd
+import numpy as np
+from itertools import chain, combinations
+import matplotlib.pyplot as plt
+from itertools import permutations
+import networkx as nx
+from mpl_toolkits.basemap import Basemap as Basemap
+
+
+
+
