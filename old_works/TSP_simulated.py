62222dd371588d04f08b6711229dc26fa55266f3
Evert Ingwersen <everti2000@gmail.com>
2025-05-08 14:24:43 +0200

    set param

--- old_works/TSP_simulated.py
+++ old_works/TSP_simulated.py
@@ -8,3 +8,11 @@ Created on Thu May  8 14:22:46 2025
 import numpy as np
 import matplotlib.pyplot as plt
 import random
+
+# Parameters
+num_cities = 20
+T_start = 1000
+T_min = 1
+alpha = 0.995
+random.seed(42)
+
