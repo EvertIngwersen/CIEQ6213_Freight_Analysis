d3ef533ed3efd3a49b37ae5e6e4f0c8657e0ccf2
Evert Ingwersen <everti2000@gmail.com>
2025-05-08 14:45:57 +0200

    made ticker loop

--- old_works/stock_ticker.py
+++ old_works/stock_ticker.py
@@ -5,4 +5,19 @@ Created on Thu May  8 14:36:02 2025
 @author: evert
 """
 
-import yfinance
+import yfinance as yf
+
+ticker = yf.Ticker("XOM")
+
+counter = []
+
+while True:
+    price = ticker.info['regularMarketPrice']
+    print("Current price:", price)
+    counter.append(1)
+    if len(counter) == 10000:
+            break
+    
+
+
+
