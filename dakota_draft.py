f470ed1e5ad83495a6873d41f46c660c3e48c3e8
Evert Ingwersen <everti2000@gmail.com>
2025-05-20 15:04:31 +0200

    made param

--- dakota_draft.py
+++ dakota_draft.py
@@ -31,3 +31,33 @@ Q_j = {"Truck1": 38,
        "Ship1": 105,
        "Ship2": 150}
 
+# Transport time of bin j (days)
+T_j = {"Truck1": 5,
+       "Truck2": 4,
+       "Plane1": 2,
+       "Ship1": 10,
+       "Ship2": 14}
+
+# Weight of container i
+w_i = {"Container1": 25, "Container2": 12, "Container3": 44, "Container4": 3,
+     "Container5": 22, "Container6": 10, "Container7": 55, "Container8": 76,
+     "Container9": 42, "Container10": 19, "Container11": 36, "Container12": 7}
+
+# Due time of container i
+D_i = {"Container1": 12, "Container2": 8, "Container3": 4, "Container4": 22,
+     "Container5": 21, "Container6": 21, "Container7": 18, "Container8": 17,
+     "Container9": 30, "Container10": 12, "Container11": 5, "Container12": 17}
+
+# Release time of container i
+A_i = {"Container1": 3, "Container2": 0, "Container3": 0, "Container4": 2,
+     "Container5": 5, "Container6": 6, "Container7": 9, "Container8": 10,
+     "Container9": 26, "Container10": 1, "Container11": 1, "Container12": 1}
+
+# A very large number for bigM >> 0 
+bigM = 1000000
+
+
+
+
+
+
