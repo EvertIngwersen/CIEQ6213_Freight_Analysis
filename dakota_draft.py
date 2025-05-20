a513761b4d75a9051d3d3c6f4667c7156bad8f58
Evert Ingwersen <everti2000@gmail.com>
2025-05-20 14:29:15 +0200

    cost and capacities

--- dakota_draft.py
+++ dakota_draft.py
@@ -6,7 +6,28 @@ Created on Tue May 20 14:09:03 2025
 """
 
 import numpy as np
+import gurobipy as gp
+from gurobipy import GRB
 
+# Sets
 N = ["Container1", "Container2", "Container3", "Container4",
-     "Container5", "Container6", "Container7", "Container8"]
-M = ["Truck1", "Truck2", "Plane1", "Ship1", "Ship2"]
+     "Container5", "Container6", "Container7", "Container8",
+     "Container9", "Container10", "Container11", "Container12"]
+M = ["Truck1", "Truck2", "Plane1", "Ship1", "Ship2"] 
+
+# Parameters
+
+# Cost of bin j
+C_j = {"Truck1": 100,
+       "Truck2": 150,
+       "Plane1": 550,
+       "Ship1": 30,
+       "Ship2": 25}
+
+# Capacity of bin j
+Q_j = {"Truck1": 38,
+       "Truck2": 35,
+       "Plane1": 75,
+       "Ship1": 105,
+       "Ship2": 150}
+
