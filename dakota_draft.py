43bb7fc8febce24f276f9a207b7e0a5b28658677
Evert Ingwersen <everti2000@gmail.com>
2025-05-20 14:23:07 +0200

    made sets q

--- dakota_draft.py
+++ dakota_draft.py
@@ -6,3 +6,7 @@ Created on Tue May 20 14:09:03 2025
 """
 
 import numpy as np
+
+N = ["Container1", "Container2", "Container3", "Container4",
+     "Container5", "Container6", "Container7", "Container8"]
+M = ["Truck1", "Truck2", "Plane1", "Ship1", "Ship2"]
