23c9dde76ebf02b3b4fd484dd26d4cf8fdcca995
Evert Ingwersen <everti2000@gmail.com>
2025-05-26 14:04:22 +0200

    cleaned csv

--- old_works/oil_commodity_trading_part3.py
+++ old_works/oil_commodity_trading_part3.py
@@ -5,3 +5,8 @@ Created on Mon May 26 13:59:56 2025
 @author: evert
 """
 
+import pandas as pd
+
+file_path = r"C:\Users\evert\Documents\TU-Delft\TIL Master\CIEQ6213 Freight Transport Networks and Systems\CIEQ6213_Freight_Analysis\old_works\oil_csv.csv"
+
+df = pd.read_csv(file_path)
